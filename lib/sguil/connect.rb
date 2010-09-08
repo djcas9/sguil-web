7f956b5859d08afbea01f925175ce35325728e13
Dustin Willis Webber <dustin.webber@gmail.com>
2010-09-08 16:43:36 -0400

    Fixed route issue

--- lib/sguil/connect.rb
+++ lib/sguil/connect.rb
@@ -68,17 +68,17 @@ module Sguil
 
         case line
         when %r|^NewSnortStats|
-          push 'path_here', format_and_publish(:new_snort_stats, line)
+          push 'sensor/updates', format_and_publish(:new_snort_stats, line)
         when %r|^SensorList|
           format_and_publish(:sensors, line)
         when %r|^UserMessage|
-          push '/user/message', format_and_publish(:user_message, line)
+          push 'user/message', format_and_publish(:user_message, line)
         when %r|^InsertSystemInfoMsg|
-          push '/system/message', format_and_publish(:insert_system_information, line)
+          push 'system/message', format_and_publish(:insert_system_information, line)
         when %r|^UpdateSnortStats|
-          push '/sensor/updates', format_and_publish(:update_snort_stats, line)
+          push 'sensor/updates', format_and_publish(:update_snort_stats, line)
         when %r|^InsertEvent|
-          push '/events/insert', format_and_publish(:insert_event, line)
+          push 'events/insert', format_and_publish(:insert_event, line)
         end
       end
     end
